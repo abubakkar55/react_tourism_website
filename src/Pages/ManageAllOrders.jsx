872b6fbfc99dd507b06e8d27d5c0633248099d86
Ab <computerm246@gmail.com>
2021-11-01 18:17:49 +0600

    responsicve done

--- src/Pages/ManageAllOrders.jsx
+++ src/Pages/ManageAllOrders.jsx
@@ -5,7 +5,7 @@ const ManageAllOrders = () => {
 
     const [myOrders, setMyOrders] = useState([]);
     useEffect(() => {
-        axios.get("https://safe-headland-90262.herokuapp.com/all__orders")
+        axios.get("https://safe-headland-90262.herokuapp.com/user_orders_collection")
             .then(res => {
                 setMyOrders(res.data);
                 console.log(res)
