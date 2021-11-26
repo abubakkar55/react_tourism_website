db0a778e5fa006b071e2a829459e3b26c1967a2c
Ab <computerm246@gmail.com>
2021-11-26 15:25:47 +0600

    loading effect added

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -13,16 +13,30 @@ const SliderComponent = () => {
         <div className="slider">
             <Carousel showArrows={true} autoPlay={true} infiniteLoop={true}>
 
-                {sliderData?.map(item => {
-                    return (
-                        <div key={item?._id} className="slider-item h-screen flex items-center flex-col justify-center" style={{ backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.36), rgba(0, 0, 0, 0.36)), url(${item?.image})`, backgroundRepeat: "no-repeat", backgroundSize: "100% 100%" }}>
-                            <h1 className="text-3xl md:text-4xl lg:text-6xl font-semibold text-white mb-7"> {item.name} </h1>
-                            <button className="outline-none px-6 py-3 border-2 border-puerto-600 text-white hover:bg-puerto-600">
-                                <NavLink to="/tour">Explore Now </NavLink>
-                            </button>
-                        </div>
-                    )
-                })}
+                {
+                    !sliderData.length > 0 ?
+
+                        (
+
+                            sliderData?.map(item => {
+                                return (
+                                    <div key={item?._id} className="slider-item h-screen flex items-center flex-col justify-center" style={{ backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.36), rgba(0, 0, 0, 0.36)), url(${item?.image})`, backgroundRepeat: "no-repeat", backgroundSize: "100% 100%" }}>
+                                        <h1 className="text-3xl md:text-4xl lg:text-6xl font-semibold text-white mb-7"> {item.name} </h1>
+                                        <button className="outline-none px-6 py-3 border-2 border-puerto-600 text-white hover:bg-puerto-600">
+                                            <NavLink to="/tour">Explore Now </NavLink>
+                                        </button>
+                                    </div>
+                                )
+                            })
+                        )
+
+                        : <h1>...Loading </h1>
+
+
+
+
+
+                }
 
             </Carousel>
         </div>
